ad3c001b9e740a0069b5b1890b16a8e36b0837aa
maeric2002 <32048160+maeric2002@users.noreply.github.com>
2020-01-17 10:17:41 -0500

    Initial commit

---
+++ Stuff.java
@@ -0,0 +1,24 @@
+// Example class for Git exercise
+// Do not use library functions for any of these
+
+public class Stuff {
+
+    // Returns the Fibonnaci sequence's nth number
+    // ex. 1, 1, 2, 3, 5, 8 - 2 would be the third number
+    static int fib(int n);
+
+    // Sorts a list of integers from smallest to largest
+    // Build your own - don't use a library sorting method
+    static int[] sort(int[] a);
+
+    // Returns the number of occurances of each letter (ABCD...) in a given string
+    static int[] letterCount(String s);
+
+    // Returns the GCD of two numbers
+    // Must use Euclid's algorithm
+    static int gcd(int a, int b);
+
+    // Returns all prime factors of a number
+    static int[] factorize(int a);
+
+}
